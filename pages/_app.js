2865b8d8c74554589df64b2f42c5d2dbaa4cecfc
alekspanteli <alekspanteli@rambler.ru>
2023-01-17 10:38:00 +0000

    add @use

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.scss'
+import Head from 'next/head'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <title>From _app.js</title>
+      </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Head from 'next/head'
+
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+    {/* <Head>
+        <title>My pageasd title</title>
+      </Head> */}
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  )
 }
