8735bdc41c2ab00e4a504702980cdddf5edd482d
alekspanteli <alekspanteli@rambler.ru>
2023-01-18 14:56:06 +0000

    add paritials with namespaces

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,10 @@ export default function HomePage() {
         <title>Home</title>
       </Head>
       <div className="container">
-        <h1 className="text-3xl font-bold underline border  border-sky-500">Hello world!</h1>
+        <h1 className="">Hello world!</h1>
+        <div className="card">
+          test
+        </div>
       </div>
     </>
   );
